4a6f8144141dd90909222c8185f48a448795c494
MICHELLCGs <michell.condori@tecsup.edu.pe>
2024-10-17 21:16:28 -0500

    db nuevos cambios

--- bulletin-board-db/init-db.sql
+++ bulletin-board-db/init-db.sql
@@ -1,20 +1,29 @@
+-- Crear la base de datos BulletinBoard
 CREATE DATABASE BulletinBoard;
 GO
 
+-- Usar la base de datos BulletinBoard
 USE BulletinBoard;
+GO
 
+-- Crear la tabla Events
 CREATE TABLE Events (
   Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
   Title NVARCHAR(50),
   Detail NVARCHAR(200),
   [Date] DATETIMEOFFSET,
-  CreatedAt DATETIMEOFFSET NOT NULL, 
-  UpdatedAt DATETIMEOFFSET NOT NULL
+  CreatedAt DATETIMEOFFSET NOT NULL DEFAULT SYSDATETIMEOFFSET(),  -- Usar la fecha y hora actual
+  UpdatedAt DATETIMEOFFSET NOT NULL DEFAULT SYSDATETIMEOFFSET()   -- Usar la fecha y hora actual
 );
+GO
 
+-- Insertar datos en la tabla Events
 INSERT INTO Events (Title, Detail, [Date], CreatedAt, UpdatedAt) VALUES
-(N'Docker for Beginners', N'Introduction to Docker using Node.js', '2017-11-21', GETDATE(), GETDATE()),
-(N'Advanced Orchestration', N'Deep dive into Docker Swarm', '2017-12-25', GETDATE(), GETDATE()),
-(N'Docker on Windows', N'From 101 to production', '2018-01-01', GETDATE(), GETDATE());
+(N'Docker for Beginners', N'Introduction to Docker using Node.js', '2017-11-21', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET()),
+(N'Advanced Orchestration', N'Deep dive into Docker Swarm', '2017-12-25', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET()),
+(N'Docker on Windows', N'From 101 to production', '2018-01-01', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET());
+GO
 
-SELECT * FROM BulletinBoard.dbo.Events;
+-- Seleccionar todos los registros de la tabla Events
+SELECT * FROM Events;
+GO
--- node-bulletin-board-2/bulletin-board-db/init-db.sh
+++
@@ -1,3 +0,0 @@
-sleep 30s
-
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'D0cker2*2*' -i init-db.sql
